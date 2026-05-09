8a66194f024221395c8f3b38e0401d7dd990a6ae
Pedro H. Lopes <pedro_hlopes@outlook.com>
2026-05-09 19:47:12 -0300

    feat: create file to generate customers

---
+++ src/generate_customers.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from faker import Faker
+from uuid import uuid4 # use uuid4 to generate unique customer IDs
+
+fake = Faker()
+
+np.random.seed(42) # for reproducibility
+
+N_CUSTOMERS = 10_000
