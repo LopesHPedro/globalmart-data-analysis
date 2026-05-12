d8f748a788bb11f5c09bdfaf73c085d881fab41d
Pedro H. Lopes <pedro_hlopes@outlook.com>
2026-05-12 16:39:12 -0300

    feat: generate countries, birth dates and register dates

--- src/generate_customers.py
+++ src/generate_customers.py
@@ -11,3 +11,27 @@ N_CUSTOMERS = 10_000
 
 customers_ids = [str(uuid4()) for _ in range(N_CUSTOMERS)]
 customers_names = [fake.name() for _ in range(N_CUSTOMERS)]
+customers_emails = [fake.email() for _ in range(N_CUSTOMERS)]
+
+countries = ["Brazil", "United States", "Canada", "France", "Spain"]
+country_weights = [0.4, 0.3, 0.1, 0.1, 0.1] # Brazil is more likely to be the country of residence
+
+customers_countries = np.random.choice(
+    countries,
+    size=N_CUSTOMERS,
+    p=country_weights
+)
+
+customers_birth_dates = [
+    fake.date_of_birth(minimum_age=18, maximum_age=80)
+    for _ in range(N_CUSTOMERS)
+]
+
+customers_registration_dates = [
+    fake.date_between(start_date="-3y", end_date="today")
+    for _ in range(N_CUSTOMERS)
+]
+
+print(customers_birth_dates[:5])
+print()
+print(customers_registration_dates[:5])
