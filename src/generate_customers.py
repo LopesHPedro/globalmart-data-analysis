ef9cc0909c4d4eefce2fb9d6aee09404fbade62a
Pedro H. Lopes <pedro_hlopes@outlook.com>
2026-05-17 02:35:09 -0300

    feat: Generate DataFrame

--- src/generate_customers.py
+++ src/generate_customers.py
@@ -3,6 +3,12 @@ import numpy as np
 from faker import Faker
 from uuid import uuid4 # use uuid4 to generate unique customer IDs
 
+def generate_email_from_name(name):
+    # Generate an email address based on the customer's name
+    username = name.lower().replace(" ", np.random.choice([".", "_", ""]))
+    domain = np.random.choice(["gmail.com", "hotmail.com", "yahoo.com", "outlook.com"])
+    return f"{username}@{domain}"
+
 fake = Faker()
 
 np.random.seed(42) # for reproducibility
@@ -11,7 +17,7 @@ N_CUSTOMERS = 10_000
 
 customers_ids = [str(uuid4()) for _ in range(N_CUSTOMERS)]
 customers_names = [fake.name() for _ in range(N_CUSTOMERS)]
-customers_emails = [fake.email() for _ in range(N_CUSTOMERS)]
+customers_emails = [generate_email_from_name(name) for name in customers_names]
 
 countries = ["Brazil", "United States", "Canada", "France", "Spain"]
 country_weights = [0.4, 0.3, 0.1, 0.1, 0.1] # Brazil is more likely to be the country of residence
@@ -32,6 +38,14 @@ customers_registration_dates = [
     for _ in range(N_CUSTOMERS)
 ]
 
+"""
 print(customers_birth_dates[:5])
 print()
 print(customers_registration_dates[:5])
+"""
+
+customers_df = pd.DataFrame({"customer_id": customers_ids, "customer_name": customers_names, "customer_email": customers_emails,
+                              "customer_country": customers_countries, "customer_birth_date": customers_birth_dates,
+                              "customer_registration_date": customers_registration_dates})
+
+print(customers_df.head())
