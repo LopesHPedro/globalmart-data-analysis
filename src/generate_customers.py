4d8acdea2c61fe06b3fc5a2718fb9a0f1c580242
Pedro H. Lopes <pedro_hlopes@outlook.com>
2026-05-27 16:55:47 -0300

    feat: create synthetic customers dataset

--- src/generate_customers.py
+++ src/generate_customers.py
@@ -19,6 +19,14 @@ customers_ids = [str(uuid4()) for _ in range(N_CUSTOMERS)]
 customers_names = [fake.name() for _ in range(N_CUSTOMERS)]
 customers_emails = [generate_email_from_name(name) for name in customers_names]
 
+# To make more realistic, we can introduce some invalid email addresses
+invalid_emails = [fake.word() for _ in range(int(N_CUSTOMERS * 0.05))] # 5% invalid emails
+
+# Now we randomly replace some of the valid emails with invalid ones
+for i in np.random.choice(N_CUSTOMERS, size=int(N_CUSTOMERS * 0.05), replace=False):
+    customers_emails[i] = np.random.choice(invalid_emails)
+
+
 countries = ["Brazil", "United States", "Canada", "France", "Spain"]
 country_weights = [0.4, 0.3, 0.1, 0.1, 0.1] # Brazil is more likely to be the country of residence
 
@@ -32,6 +40,13 @@ customers_birth_dates = [
     fake.date_of_birth(minimum_age=18, maximum_age=80)
     for _ in range(N_CUSTOMERS)
 ]
+
+MISSING_BIRTH_DATES = int(N_CUSTOMERS * 0.03) # 3% missing birth dates
+
+missing_birth_date_index = np.random.choice(N_CUSTOMERS, size=int(MISSING_BIRTH_DATES), replace=False)
+
+for i in missing_birth_date_index:
+    customers_birth_dates[i] = None
 
 customers_registration_dates = [
     fake.date_between(start_date="-3y", end_date="today")
@@ -48,4 +63,5 @@ customers_df = pd.DataFrame({"customer_id": customers_ids, "customer_name": cust
                               "customer_country": customers_countries, "customer_birth_date": customers_birth_dates,
                               "customer_registration_date": customers_registration_dates})
 
-print(customers_df.head())
+customers_df.to_csv("data/raw/customers.csv", index=False)
+print("customers.csv generated successfully!")
