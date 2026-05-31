9b8bd210f8e895da9c7f52cc38b76b6e583e9b59
Pedro H. Lopes <pedro_hlopes@outlook.com>
2026-05-31 08:59:37 -0300

    feat: define product catalog structure

--- src/generate_products.py
+++ src/generate_products.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-np.random.seed(42)
+np.random.seed() # For reproducibility
 
 N_PRODUCTS = 500
 
@@ -13,3 +13,19 @@ categories ={
     "Fashion": ["T-Shirt", "Jeans", "Sneakers", "Jacket", "Dress"],
     "Sports": ["Football", "Yoga Mat", "Running Shoes", "Dumbbell", "Backpack"]
 }
+
+categories_weights = {
+    "Books": 0.15,
+    "Electronics": 0.30, # Globalmart has a strong focus on electronics
+    "Makeup": 0.10,
+    "Garden": 0.05,
+    "Fashion": 0.20,
+    "Sports": 0.20
+}
+
+prefixes = ["Super", "Ultra", "Mega", "Eco", "Pro", "Smart", "Easy", "Quick", "Best", "New"]    
+suffixes = ["X", "Plus", "Max", "Pro", "Lite", "Go", "Air", "One", "Prime", "Elite"]
+
+# Teste
+selected_category = np.random.choice(list(categories.keys()), p=list(categories_weights.values())) # Only works because the keys and values are in the same order
+print(np.random.choice(prefixes),np.random.choice(categories[selected_category]), np.random.choice(suffixes))
